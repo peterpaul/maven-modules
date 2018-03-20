44cc95d745dc392c25f4b849355382137f3a71b9
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@connectis.nl>
2018-03-20 14:23:46 +0100

    Generate dependency graph

--- src/main/kotlin/net/kleinhaneveld/tree/PomParser.kt
+++ src/main/kotlin/net/kleinhaneveld/tree/PomParser.kt
@@ -1,16 +1,41 @@
 package net.kleinhaneveld.tree
 
+import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import java.io.File
+import java.io.FileFilter
 import javax.xml.parsers.DocumentBuilderFactory
 
 data class MavenCoordinate (
         val groupId: String,
         val artifactId: String,
-        val version: String
-        )
+        val version: String?,
+        val scope: String?
+)
+
+data class MavenPom (
+        val parent: MavenCoordinate?,
+        val coordinate: MavenCoordinate,
+        val dependencies: List<MavenCoordinate>
+)
+
+data class MavenVertex (
+        val groupId: String,
+        val artifactId: String
+) {
+    override fun toString(): String {
+        return "$groupId:$artifactId"
+    }
+}
+
+fun MavenCoordinate.toVertex(): MavenVertex = MavenVertex(this.groupId, this.artifactId)
+
+data class MavenEdge (
+        override val parent: MavenVertex,
+        override val child: MavenVertex
+) : Edge<MavenVertex>
 
 val documentBuilderFactory = DocumentBuilderFactory.newInstance()
 
@@ -31,32 +56,31 @@ class NodeListIterator(private val nodeList: NodeList) : Iterator<Node> {
 }
 
 fun NodeList.iterator(): Iterator<Node> = NodeListIterator(this)
-
 fun NodeList.asSequence(): Sequence<Node> = Sequence { this.iterator() }
-
-fun Element.single(tagName: String): Node = this.getElementsByTagName(tagName).asSequence().single()
-
-fun Element.trySingle(tagName: String): Node? = this.getElementsByTagName(tagName).asSequence().singleOrNull()
-
-fun Node.single(tagName: String): Node = (this as Element).single(tagName)
+fun Node.single(tagName: String): Node = this.trySingle(tagName) ?: throw IllegalStateException("Expected single, but got no or multiple $tagName's")
 
 fun Node.trySingle(tagName: String): Node? = when (this) {
-    is Element -> this.single(tagName)
+    is Element -> this.childNodes
+            .asSequence()
+            .filterIsInstance<Element>()
+            .filter { it.nodeName == tagName }
+            .singleOrNull()
     else -> null
 }
 
 fun Node.groupId(): String = this.single("groupId").textContent
 fun Node.artifactId(): String = this.single("artifactId").textContent
-fun Node.version(): String = this.single("version").textContent
 fun Node?.tryGroupId(): String? = this?.trySingle("groupId")?.textContent
 fun Node?.tryArtifactId(): String? = this?.trySingle("artifactId")?.textContent
 fun Node?.tryVersion(): String? = this?.trySingle("version")?.textContent
+fun Node?.tryScope(): String? = this?.trySingle("scope")?.textContent
 
 fun Node.mavenCoordinate(): MavenCoordinate {
     return MavenCoordinate(
             this.groupId(),
             this.artifactId(),
-            this.version()
+            this.tryVersion(),
+            this.tryScope()
     )
 }
 
@@ -64,15 +88,67 @@ fun Node.mavenCoordinateUsingDefault(defaultMavenCoordinate: MavenCoordinate): M
     return MavenCoordinate(
             this.tryGroupId() ?: defaultMavenCoordinate.groupId,
             this.tryArtifactId() ?: defaultMavenCoordinate.artifactId,
-            this.tryVersion() ?: defaultMavenCoordinate.version
+            this.tryVersion() ?: defaultMavenCoordinate.version,
+            this.tryScope()
     )
 }
 
-fun parsePom(fileName: File) {
+fun Node.tryChildrenOfSingle(tagName: String): Sequence<Node> = this.trySingle(tagName)?.childNodes?.asSequence() ?: emptySequence()
+fun Node.dependencies(): Sequence<Element> = this.tryChildrenOfSingle("dependencies").filterIsInstance<Element>()
+
+fun Document.mavenPom(): MavenPom {
+    val project = this.documentElement
+    val parent: MavenCoordinate? = project.trySingle("parent")?.mavenCoordinate()
+    val pomCoordinate: MavenCoordinate = if (parent != null) {
+        project.mavenCoordinateUsingDefault(parent)
+    } else {
+        project.mavenCoordinate()
+    }
+    val dependencies: List<MavenCoordinate> = project
+            .dependencies()
+            .map { it.mavenCoordinate() }
+            .toList()
+    return MavenPom(parent, pomCoordinate, dependencies)
+}
+
+fun parsePom(fileName: File): MavenPom {
     val pomDocument = documentBuilderFactory.newDocumentBuilder().parse(fileName)
-    val project = pomDocument.documentElement
-    val parent = project.trySingle("parent")
-    val parentCoordinate = parent!!.mavenCoordinate()
-    println(parentCoordinate)
-    println(project.mavenCoordinateUsingDefault(parentCoordinate))
+    val out = pomDocument.mavenPom()
+    println(out)
+    return out
+}
+
+fun parseDir(fileName: File): List<MavenPom> {
+    return if (fileName.isDirectory) {
+        fileName.listFiles(FileFilter { it.isDirectory && File(it, "pom.xml").exists() })
+                .flatMap { parseDir(it) }
+                .toList() + parsePom(File(fileName, "pom.xml"))
+    } else {
+        emptyList()
+    }
+}
+
+fun MavenPom.edges(): List<MavenEdge> {
+    val parent = this.coordinate.toVertex()
+    val dependencies = this.dependencies.map { MavenEdge(parent, it.toVertex()) }
+    if (this.parent != null) {
+        return dependencies + MavenEdge(parent, this.parent.toVertex())
+    } else {
+        return dependencies
+    }
+}
+
+fun main(args: Array<String>) {
+    val poms = parseDir(File("/home/peterpaulk/projects/federation"))
+    println("size: ${poms.size}")
+
+    val vertices: Set<MavenVertex> = poms.map { it.coordinate.toVertex() }.toSet()
+    println("vertices size = ${vertices.size}")
+    val edges: Set<MavenEdge> = poms.flatMap { it.edges() }.filter { it.parent in vertices && it.child in vertices }.toSet()
+    println("edges size = ${edges.size}")
+    val root = poms.last().coordinate.toVertex()
+    println("root: $root")
+    val graph = Graph(vertices, edges, root)
+
+    vertices.filter { graph.orderIncoming(it) == 0 }.forEach { println(it) }
 }
